641445e4bc6b815f46f9fae49350fd1567360cff
keir-l <41842366+keir-l@users.noreply.github.com>
2018-11-28 11:38:13 +0000

    Publisher Detection & Automatic Redirection
    Added publisher detection and automatic redirection

--- background.js
+++ background.js
@@ -7,3 +7,27 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 		file: 'proxy.js'
 	});
 });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+			var url = tabs[0].url;
+			function getHostName(url) {
+			    var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+			    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+			    return match[2];
+			    }
+			    else {
+			        return null;
+			    }
+			}
+			var hostname = getHostName(url);
+		 var publisherURLS = ["tandfonline.com", "jstor.org", "heinonline.org"];
+		 if (publisherURLS.indexOf(hostname) > -1) {
+		     console.log("Publisher " + hostname + " found, redirecting...");
+				 chrome.tabs.executeScript(tab.ib, {
+			 		file: 'proxy.js'
+			 	});
+		 } else {
+		     console.log(hostname + " does not need to be redirected");
+		 }
+	});
+});
