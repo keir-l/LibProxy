40c710c2aa450060dbd13eca7bbb95fe379e480a
keir-l <41842366+keir-l@users.noreply.github.com>
2018-11-30 13:39:35 +0000

    Updated springlink

--- background.js
+++ background.js
@@ -20,7 +20,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 			    }
 			}
 			var hostname = getHostName(url);
-		 var publisherURLS = ["journals.sagepub.com", "cambridge.org", "tandfonline.com", "jstor.org", "heinonline.org", "academic.oup.com"];
+		 var publisherURLS = ["link.springer.com", "journals.sagepub.com", "cambridge.org", "tandfonline.com", "jstor.org", "heinonline.org", "academic.oup.com"];
 		 if (publisherURLS.indexOf(hostname) > -1) {
 				 chrome.tabs.executeScript(tab.ib, {
 			 		file: 'proxy.js'
