e7c66d2e916cdd9a38469fbb8047986b4a4a0a5c
keir-l <41842366+keir-l@users.noreply.github.com>
2018-12-05 18:01:57 +0000

    Fixed match empty var error

--- background.js
+++ background.js
@@ -11,6 +11,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
 			var url = tabs[0].url;
 			function getHostName(url) {
+				if (url) {
 			    var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
 			    if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
 			    return match[2];
@@ -18,7 +19,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 			    else {
 			        return null;
 			    }
-			}
+			}}
 			var hostname = getHostName(url);
 		 var publisherURLS = ["link.springer.com", "journals.sagepub.com", "cambridge.org", "tandfonline.com", "jstor.org", "heinonline.org", "academic.oup.com"];
 		 if (publisherURLS.indexOf(hostname) > -1) {
